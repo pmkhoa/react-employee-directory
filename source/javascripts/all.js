c09ff9090dc9a1740886b89fa7ce470661d2e47b
Khoa Pham <khoa.pham@me.com>
2015-05-15 17:33:36 -0700

    Hello World with React
    When including jsx javascript, make sure to use the tag: <script
    type="text/jsx" >
    
    We can rendering component to the target DOM element by passing
    document.getElementByID

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
-//= require_tree .
+var HelloWorld = React.createClass({
+    render: function () {
+        return (
+            <h1 className="title">{this.props.text}</h1>
+        );
+    }
+});
+
+React.render(
+  <HelloWorld text="Hello World" />,
+  document.getElementById("container")
+);
