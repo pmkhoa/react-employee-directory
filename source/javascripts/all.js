ed86ab1d7406baa56bbf3ec019e5dc9d61058ec4
Khoa Pham <khoa.pham@me.com>
2015-05-15 22:11:14 -0700

    Create simple Homepage by using Component.
    Including: Header, SearchBar, and EmployeeList

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,43 @@
-var HelloWorld = React.createClass({
-    render: function () {
-        return (
-            <h1 className="title">{this.props.text}</h1>
-        );
-    }
+var Header = React.createClass({
+  render: function() {
+    return (
+      <h1 class="title">{this.props.text}</h1>
+    );
+  }
+});
+
+var SearchBar = React.createClass({
+  render: function() {
+    return (
+      <input type="search" class="searchbar" />
+    );
+  }
+});
+
+var EmployeeList = React.createClass({
+  render: function() {
+    return (
+      <ul>
+        <li>Khoa Pham</li>
+        <li>Uyen Phan</li>
+      </ul>
+    );
+  }
+});
+
+var HomePage = React.createClass({
+  render: function() {
+    return (
+      <div class="inner-container">
+        <Header text="Employee Directory"/>
+        <SearchBar />
+        <EmployeeList />
+      </div>
+    );
+  }
 });
 
 React.render(
-  <HelloWorld text="Hello World" />,
+  <HomePage />,
   document.getElementById("container")
 );
